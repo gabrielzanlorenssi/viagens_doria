2033682d971fdab5d45ba88989cc532b0646de1c
Gabriel Zanlorenssi <gabriel.zanlorenssi@nexojornal.com.br>
2018-02-11 14:56:57 -0200

    updated code

--- doria.R
+++ doria.R
@@ -1,4 +1,4 @@
-#libraries
+# Libraries
 
 library(dplyr)
 library(ggplot2)
@@ -6,21 +6,22 @@ library(readxl)
 library(ggthemes)
 library(lubridate)
 
-#doria
+# Gerar a imagem
 
-doria <- read_excel("~/Documents/doria.xlsx") %>% 
+doria <- read_excel("./doria.xlsx") %>% 
   mutate(day=day(date),
          month=month(date),
          weekday=weekdays(date), 
          week=week(date)) %>% 
   filter(month!=12) %>% 
-  mutate(weekday2=ifelse(weekday=="Domingo", 1, ifelse(
-                        weekday=="Segunda Feira", 2, ifelse(
-                        weekday=="Terça Feira", 3, ifelse(
-                        weekday=="Quarta Feira", 4, ifelse(
-                        weekday=="Quinta Feira", 5, ifelse(
-                        weekday=="Sexta Feira", 6, ifelse(
-                        weekday=="Sábado", 7, NA)))))))) %>% 
+  mutate(weekday2=case_when(weekday=="Domingo" ~ 1,
+                        weekday=="Segunda Feira" ~ 2,
+                        weekday=="Terça Feira" ~ 3,
+                        weekday=="Quarta Feira" ~ 4,
+                        weekday=="Quinta Feira" ~ 5, 
+                        weekday=="Sexta Feira" ~ 6, 
+                        weekday=="Sábado" ~ 7, 
+                        TRUE ~ NA)) %>% 
   ggplot(aes(x=reorder(substr(weekday, 1, 3),
                        weekday2), y=reorder(week, -week))) +
   geom_tile(aes(fill=as.factor(travel)), size=2) +
@@ -29,6 +30,8 @@ doria <- read_excel("~/Documents/doria.xlsx") %>%
   theme_tufte() +
   scale_fill_manual(values=c("#bdbdbd", "#e41a1c")) +
   labs(x="", y="")
+
+# Salvar como png 
 
 ggsave("doria.png", plot=doria, width=8.4, height = 6.4, dpi=300)
 
